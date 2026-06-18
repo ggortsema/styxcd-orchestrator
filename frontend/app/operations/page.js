e6c0a330dd84901d75a0eded4ce4ad7cf3df29a7
Grant Gortsema <grant.gortsema@gmail.com>
2026-06-18 18:39:46 -0400

    compacted the spaces

--- frontend/app/operations/page.js
+++ frontend/app/operations/page.js
@@ -5,66 +5,40 @@ import { useEffect, useRef, useState } from 'react';
 const API_BASE_URL = 'http://orchestrator.styxcd.com';
 
 const defaultYml = `workflow: cloud_workflow
-
 release:
   name: johnny-platform-release
   version: 1.0.0
-
   desired_state:
     environment: present
     applications: deployed
-
   applications:
-
     spring:
       - name: johnny-johnny-backend
         repo: https://github.com/ggortsema/johnny-johnny.git
         branch: main
         version: 1.0.0
-
         build:
           type: maven-docker
           project_path: .
           module_path: chat-api
           dockerfile: chat-api/Dockerfile
           docker_context: .
-
         artifacts:
           - type: docker-image
             image: us-east1-docker.pkg.dev/styxcd-sandbox-grant/styxcd-sandbox/johnny-johnny-backend:latest
-
-      - name: johnny-johnny-backend-2
-        repo: https://github.com/ggortsema/johnny-johnny.git
-        branch: main
-        version: 1.0.0
-
-        build:
-          type: maven-docker
-          project_path: .
-          module_path: chat-api
-          dockerfile: chat-api/Dockerfile
-          docker_context: .
-
-        artifacts:
-          - type: docker-image
-            image: us-east1-docker.pkg.dev/styxcd-sandbox-grant/styxcd-sandbox/johnny-johnny-backend:latest
-
     node:
       - name: johnny-johnny-ui
         repo: https://github.com/ggortsema/johnny-johnny-ui.git
         branch: main
         version: 1.0.0
-
         build:
           type: docker
           project_path: .
           dockerfile: Dockerfile
           docker_context: .
-
         artifacts:
           - type: docker-image
             image: us-east1-docker.pkg.dev/styxcd-sandbox-grant/styxcd-sandbox/johnny-johnny-ui:latest
-
   environments:
     sandbox:
       - name: johnny-johnny-gke-sandbox
@@ -75,16 +49,13 @@ release:
           location: us-east1-b
           location_type: zonal
           namespace: johnny-johnny
-
           credentials:
             source: jenkins
             id: gcp-service-account
-
           defaults:
             replicas: 1
             service:
               type: ClusterIP
-
           ingress:
             enabled: true
             name: johnny-johnny-ingress
@@ -96,14 +67,12 @@ release:
                     path_type: Prefix
                     service: johnny-johnny-ui
                     port: 80
-
               - host: api.johnny-johnny.mycroftai.org
                 routes:
                   - path: /
                     path_type: Prefix
                     service: johnny-johnny-backend
                     port: 80
-
           dns:
             enabled: true
             provider: route53
@@ -115,7 +84,6 @@ release:
               source: jenkins
               access_key_id: aws-access-key-id
               secret_access_key: aws-secret-access-key
-
           applications:
             - name: johnny-johnny-backend
               container:
@@ -132,7 +100,6 @@ release:
                   source:
                     type: jenkins-credential
                     credential_id: openai-api-key
-
             - name: johnny-johnny-ui
               container:
                 port: 3000
